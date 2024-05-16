40aef07e756361c9c489086e585ceef24e7f3c9a
NadaMamdouh225 <105312001+NadaMamdouh225@users.noreply.github.com>
2024-05-16 22:33:59 +0300

    fix sending message bug

--- pkg_rp_arm/pkg_rp_arm/script_rp_arm.py
+++ pkg_rp_arm/pkg_rp_arm/script_rp_arm.py
@@ -4,7 +4,6 @@ from rclpy.node import Node
 from geometry_msgs.msg import TransformStamped
 import tf2_ros
 from sensor_msgs.msg import JointState
-from moveit_msgs.srv import GetPositionFK
 from geometry_msgs.msg import Twist
 import math
 import time
@@ -16,23 +15,21 @@ class my_Node(Node):
         self.jointPublisher = self.create_publisher(JointState,"/joint_states",10)
 
         #self.keyboardSubscriber = self.create_subscription(Twist,"/cmd_vel", self.key_input_callback, 10)
-
-        self.fk_client = self.create_client(GetPositionFK, 'compute_fk')
         
         self.tf_broadcaster_ = tf2_ros.TransformBroadcaster(self)
 
         self.jointName = ["slider_joint", "arm_joint"]
-        #self.jointPosition = [-0.4, 0.0]
+
         self.jointPosition = [0.0, 0.0]
         self.z = 0.0
         self.w = 1.0
         self.get_logger().info("RP_Arm node has been started")
         
-        self.timer1 = self.create_timer(0.1, self.key_input_callback)
-        self.timer2 = self.create_timer(0.01, self.go_to_target)
+        self.timer = self.create_timer(0.1, self.go_to_target)
 
     def go_to_target(self):
         #[self.p, self.r] = self.get_input()
+        self.key_input_callback()
         msg = JointState()
         timestamp = time.time()
         seconds = int(timestamp)
@@ -61,8 +58,6 @@ class my_Node(Node):
         msg_base_arm.transform.rotation.z = self.z
         msg_base_arm.transform.rotation.w = self.w
 
-        self.tf_broadcaster_.sendTransform(msg_base_arm)
-
         # Publish transform for rod to slider
         msg_rod_slider = TransformStamped()
         msg_rod_slider.header.stamp.sec = seconds
@@ -79,7 +74,7 @@ class my_Node(Node):
         msg_rod_slider.transform.rotation.z = 0.0
         msg_rod_slider.transform.rotation.w = 1.0
 
-        self.tf_broadcaster_.sendTransform(msg_rod_slider)
+        self.tf_broadcaster_.sendTransform(msg_base_arm)
 
     def key_input_callback(self):  
         '''       
@@ -100,14 +95,14 @@ class my_Node(Node):
                 self.jointPosition[0] += 0.0
             '''
         [self.dist, self.theta] = self.inverse_kinematics()
-        #if self.dist >= -0.6 and self.dist <= 0.6:
-        self.jointPosition[0] = self.dist
-        #if math.radians(self.theta) >= 0 and math.radians(self.theta) <= 6.28:
-        self.jointPosition[1] = self.theta
+        if self.dist >= -0.4 and self.dist <= 1.0:
+            self.jointPosition[0] = self.dist
+        if math.radians(self.theta) >= 0 and math.radians(self.theta) <= 6.28:
+            self.jointPosition[1] = self.theta
         
     def inverse_kinematics(self):
         [self.x, self.y] = self.get_input()
-       
+    
         if self.y == 0.0:
             self.y = 0.00001
         self.dist = math.sqrt(pow(self.x , 2) + pow(self.y , 2))     
@@ -118,25 +113,24 @@ class my_Node(Node):
             self.theta = (3*3.14/2) - abs(self.theta)
         elif(self.x > 0 and self.y<0):
             self.theta =(3*3.14/2) + abs(self.theta)
-            
+        
         [self.z , self.w] = self.calculate_quaternion(self.theta)
         return self.dist, self.theta
-
+    
  
     def get_input(self):
         print("Enter end effector position")
         self.x = float(input("x:"))
         self.y = float(input("y:"))
-        #self.x +=0.2
         return self.x , self.y
 
     def calculate_quaternion(self,theta_rad):
-        self.w = math.cos(math.degrees(theta_rad/2))
-        self.z = math.sin(math.degrees(theta_rad/2))
-        #if(theta_rad >= 0 and theta_rad <= 2.093):
-        #    self.z = -self.z
-        #elif(theta_rad > 2.093 and theta_rad <= 3.14):
-        #    self.w = -self.w
+        self.w = (math.cos(math.degrees(theta_rad/2)))
+        self.z = (math.sin(math.degrees(theta_rad/2)))
+        if(theta_rad >= 0 and theta_rad <= 2.093):
+            self.z = -self.z
+        elif(theta_rad > 2.093 and theta_rad <= 3.14):
+            self.w = -self.w
         return self.z , self.w
 
 
