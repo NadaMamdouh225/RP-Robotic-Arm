034d61c581c4363b213b13e376097c60c68a3e5f
NadaMamdouh225 <105312001+NadaMamdouh225@users.noreply.github.com>
2024-06-04 15:53:50 +0300

    fix quaternion function

--- pkg_rp_arm/pkg_rp_arm/script_rp_arm.py
+++ pkg_rp_arm/pkg_rp_arm/script_rp_arm.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
-from geometry_msgs.msg import TransformStamped
+from geometry_msgs.msg import TransformStamped,Quaternion
 import tf2_ros
 from sensor_msgs.msg import JointState
-from geometry_msgs.msg import Twist
 import math
+from math import sin, cos, pi
 import time
 
 
@@ -16,9 +16,9 @@ class Move_Arm_Node(Node):
         self.tf_broadcaster_ = tf2_ros.TransformBroadcaster(self)
 
         self.jointName = ["slider_joint", "arm_joint"]
+        # Define initial values 
         self.jointPosition = [0.0, 0.0]
-        self.z = 0.0
-        self.w = 1.0
+        self.theta = 0
         self.get_logger().info("RP_Arm node has been started")
         
         self.timer = self.create_timer(0.1, self.go_to_target)
@@ -41,14 +41,8 @@ class Move_Arm_Node(Node):
         msg_base_arm.header.frame_id = "base_link"
         msg_base_arm.child_frame_id = "arm_link"
 
-        msg_base_arm.transform.translation.x= 0.0
-        msg_base_arm.transform.translation.y = 0.0
         msg_base_arm.transform.translation.z = 0.7
-
-        msg_base_arm.transform.rotation.x = 0.0
-        msg_base_arm.transform.rotation.y = 0.0 
-        msg_base_arm.transform.rotation.z = self.z
-        msg_base_arm.transform.rotation.w = self.w
+        msg_base_arm.transform.rotation = euler_to_quaternion(0, 0, self.theta + pi/2)
 
         # Publish transform for rod to slider
         msg_rod_slider = TransformStamped()
@@ -58,16 +52,9 @@ class Move_Arm_Node(Node):
         msg_rod_slider.child_frame_id = "slider_link"
         
         msg_rod_slider.transform.translation.x = self.jointPosition[0]
-        msg_rod_slider.transform.translation.y = 0.0
-        msg_rod_slider.transform.translation.z = 0.0
-
-        msg_rod_slider.transform.rotation.x = 0.0
-        msg_rod_slider.transform.rotation.y = 0.0 
-        msg_rod_slider.transform.rotation.z = 0.0
         msg_rod_slider.transform.rotation.w = 1.0
-
+        
         self.tf_broadcaster_.sendTransform(msg_base_arm)
-
         self.key_input_callback()
         
         
@@ -77,15 +64,9 @@ class Move_Arm_Node(Node):
         if self.y == 0.0:
             self.y = 0.00001
         self.dist = math.sqrt(pow(self.x , 2) + pow(self.y , 2))     
-        self.theta = math.atan( self.x / self.y) 
-        if(self.x < 0 and self.y>0):
-            self.theta  = abs(self.theta) +(3.14/2)
-        elif(self.x < 0 and self.y<0):
-            self.theta = (3*3.14/2) - abs(self.theta)
-        elif(self.x > 0 and self.y<0):
-            self.theta =(3*3.14/2) + abs(self.theta)
-        
-        [self.z , self.w] = self.calculate_quaternion(self.theta)
+        self.theta = math.atan(- self.x / self.y) 
+        if(self.y < 0):
+            self.theta += 3.14
         return self.dist, self.theta
     
     def get_input(self):
@@ -93,22 +74,24 @@ class Move_Arm_Node(Node):
         self.x = float(input("x:"))
         self.y = float(input("y:"))
         return self.x , self.y
-
-    def calculate_quaternion(self,theta_rad):
-        self.w = (math.cos(math.degrees(theta_rad/2)))
-        self.z = (math.sin(math.degrees(theta_rad/2)))
-        if(theta_rad >= 0 and theta_rad <= 2.093):
-            self.z = -self.z
-        elif(theta_rad > 2.093 and theta_rad <= 3.14):
-            self.w = -self.w
-        return self.z , self.w
     
     def key_input_callback(self):  
         [self.dist, self.theta] = self.inverse_kinematics()
-        if self.dist >= -0.4 and self.dist <= 1.0:
+        if (self.dist > -1.1 and self.dist < 1.1):
             self.jointPosition[0] = self.dist
-        if math.radians(self.theta) >= 0 and math.radians(self.theta) <= 6.28:
-            self.jointPosition[1] = self.theta
+        elif(self.dist > 1.1):
+            self.jointPosition[0] = 1.0
+        elif(self.dist < -1.1):
+            self.jointPosition[0] = -1.0
+        self.jointPosition[1] = self.theta
+
+
+def euler_to_quaternion(roll, pitch, yaw):
+    qx = sin(roll/2) * cos(pitch/2) * cos(yaw/2) - cos(roll/2) * sin(pitch/2) * sin(yaw/2)
+    qy = cos(roll/2) * sin(pitch/2) * cos(yaw/2) + sin(roll/2) * cos(pitch/2) * sin(yaw/2)
+    qz = cos(roll/2) * cos(pitch/2) * sin(yaw/2) - sin(roll/2) * sin(pitch/2) * cos(yaw/2)
+    qw = cos(roll/2) * cos(pitch/2) * cos(yaw/2) + sin(roll/2) * sin(pitch/2) * sin(yaw/2)
+    return Quaternion(x=qx, y=qy, z=qz, w=qw)
 
 
 def main(args = None):
